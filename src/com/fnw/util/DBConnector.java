e73f6ed276c792ff4b7d4ca4a0d540ce262084ae
kim_home <hjun1121@naver.com>
2017-11-12 20:50:32 +0900

    회원가입  pw 확인 , 빈칸있으면 submit 불가

--- src/com/fnw/util/DBConnector.java
+++ src/com/fnw/util/DBConnector.java
@@ -16,8 +16,6 @@ public class DBConnector {
 		//String url="jdbc:oracle:thin:@192.168.0.8:1521:xe"; //희성
 		//String url="jdbc:oracle:thin:@192.168.0.9:1521:xe"; //현민
 		String url="jdbc:oracle:thin:@192.168.0.10:1521:xe"; //형준
-		//String url="jdbc:oracle:thin:@192.168.0.8:1521:xe"; //희성
-		//String url="jdbc:oracle:thin:@192.168.0.9:1521:xe"; //현민
 		String driver="oracle.jdbc.driver.OracleDriver";
 		
 		//driver
