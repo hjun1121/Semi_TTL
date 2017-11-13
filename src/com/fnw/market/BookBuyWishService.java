7794171f124360ee849010ac79d3085a791b1d8a
ssin226 <ssin226@naver.com>
2017-11-13 15:17:24 +0900

    마토리 title -> num으로 바꿔서 비교

--- src/com/fnw/market/BookBuyWishService.java
+++ src/com/fnw/market/BookBuyWishService.java
@@ -14,17 +14,23 @@ public class BookBuyWishService implements Action {
 	public ActionFoward doProcess(HttpServletRequest request, HttpServletResponse response) {
 		ActionFoward actionFoward = new ActionFoward();
 		Book_Buy_WishDAO book_Buy_WishDAO = new Book_Buy_WishDAO();
-		Market_TotalDTO market_TotalDTO = new Market_TotalDTO();
+		int num = 0;
+		try {
+			num = Integer.parseInt(request.getParameter("num"));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		Market_TotalDTO market_TotalDTO = null;
+		try {
+			market_TotalDTO = new Market_TotalDAO().selectOne(num);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		int curPage = 1;
 		try {
 			curPage = Integer.parseInt(request.getParameter("curPage"));
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		int num = 0;
-		try {
-			num = Integer.parseInt(request.getParameter("num"));
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -33,7 +39,6 @@ public class BookBuyWishService implements Action {
 			id = "";
 		}
 		int result = 0;
-		String message = null;
 		try {
 			result = book_Buy_WishDAO.bookBuyWish(market_TotalDTO, id);
 		} catch (Exception e) {
