decede7cc0d7e1808db52978c3b928bcea14d4b3
geee_hyun_Home <dlwlgus349@naver.com>
2017-11-21 15:36:36 +0900

    .

--- src/com/fnw/util/DBConnector.java
+++ src/com/fnw/util/DBConnector.java
@@ -12,16 +12,16 @@ public class DBConnector {
 		String password="library";
 		//String url="jdbc:oracle:thin:@192.168.110.34:1521:xe";
 		//String url="jdbc:oracle:thin:@192.168.20.46:1521:xe";
-<<<<<<< HEAD
+
 		//String url="jdbc:oracle:thin:@192.168.35.118:1521:xe";
-		String url="jdbc:oracle:thin:@192.168.0.9:1521:xe";  //현민
+		//String url="jdbc:oracle:thin:@192.168.0.9:1521:xe";  //현민
 		//String url="jdbc:oracle:thin:@192.168.0.8:1521:xe"; //희성
-=======
+
 		String url="jdbc:oracle:thin:@172.20.10.2:1521:xe";//젼
 		//String url="jdbc:oracle:thin:@192.168.0.9:1521:xe";  //현민
 		//String url="jdbc:oracle:thin:@192.168.0.8:1521:xe"; //희성
 		//String url="jdbc:oracle:thin:@192.168.0.10:1521:xe";  //형준
->>>>>>> 74432679258f4e153593f893580d17a3a63e96fe
+
 
 		String driver="oracle.jdbc.driver.OracleDriver";
 		
